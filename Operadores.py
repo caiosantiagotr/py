c6bf0f4bcf24171ca790e6deafee2139d4d6dde6
Caio <caiosantiago0987sg@gmail.com>
2025-03-01 20:23:28 -0300

    commit inicial

---
+++ Operadores.py
@@ -0,0 +1,16 @@
+a = 5
+b = 9
+soma = a + b #14
+# AS VARIAVEIS SÃO A E B E OS VALORES SÃO 5 E 9
+sustracao = a - b #4
+#susbstituir o segundo valor ser era 14 vai passar agora ser 4
+multiplicaçao = a * b #23
+# multiplicaçao do a e do b 
+divisao = a / b # 2.333333333
+# dividir os valores do a e do b 
+divisao_inteira = a // b # 2
+# tirar todos os valores que estao depois do ponto e o principal e o mas impontante
+modulo = a % b #1
+# devolver os valores que foram do a e do b
+exponenciacao = a ** b #2000
+# elevar o primeiro valor a pontecial do segundo
