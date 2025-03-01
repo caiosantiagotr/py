612e31eeed3578bd5dcfe64cd4f9c4df87f6669f
Caio <caiosantiago0987sg@gmail.com>
2025-03-01 20:46:29 -0300

    commit inicial

---
+++ Logicos.py
@@ -0,0 +1,17 @@
+a = 20 
+b = 12
+resultado_and = (a > 2) and (b <2) #True
+# vai ser um condiçao vendadeira por que sao os mesmos valores
+# exp:
+resultado_and = (a > 8) and (b <5) #falso
+# nao e uma condiçao vendadeira mas sdim false
+resultado_and = (a >10) or (b < 5) #True
+# condiçao vendadeira
+resultado_not = not (a > 5) #False
+# exp:
+resultado_not = not (a/10) (b =5 ) (a == 5)
+# inveter o 10
+# devolve o vendadeiro que o 5
+# valor da condiçao e o 10
+# devolve o vendadeiro que e o 5 
+# condiçao false e o 10
---
+++ comparação.py
@@ -0,0 +1,14 @@
+a = 10
+b = 9
+# nao sao valores iguais sao valores diferentes ex: (==)
+# so sao valores iguais ser forem de mesmo valor ex: (!=)
+igual = a == b # false
+# NAO SAO VALORES VENDADEIROS
+diferente = a !=b #True
+# vendadeiro
+maior = a > b #True
+menor=  a < b #False
+maior = a >= b #True
+# nao e igual ao b mas ele e maior que o b 
+menor = a <= b #False
+# ele nao e maior e nem menor e nem igual por isso ele e Falso
